6e64e3b6a20e761f5c85c5faff5d3cc9c8dc706b
Taylor Sweetman <taylor.sweetman@glia.com>
2024-12-04 11:42:33 -0500

    4-2

--- aoc04/src/main.rs
+++ aoc04/src/main.rs
@@ -7,25 +7,25 @@ use my_lib::{assert_and_print, get_input_as_string};
 type CoOrd = (i32, i32);
 type Parsed = (HashMap<CoOrd, char>, HashSet<CoOrd>);
 
-fn parse(demo_input: &bool) -> Parsed {
+fn parse(demo_input: &bool, search_char: &char) -> Parsed {
     let file_contents = get_input_as_string(demo_input);
     let lines: Vec<&str> = file_contents.lines().collect();
 
     let mut parsed_map: HashMap<CoOrd, char> = HashMap::with_capacity(lines.len() * lines[0].len());
-    let mut x_locations: HashSet<CoOrd> = HashSet::new();
+    let mut search_char_locs: HashSet<CoOrd> = HashSet::new();
 
     lines.iter().enumerate().for_each(|(y, line)| {
         line.chars().enumerate().for_each(|(x, ch)| {
             let coord = (x as i32, y as i32);
 
             parsed_map.entry(coord).or_insert(ch);
-            if ch == 'X' {
-                x_locations.insert(coord);
+            if ch == *search_char {
+                search_char_locs.insert(coord);
             }
         });
     });
 
-    (parsed_map, x_locations)
+    (parsed_map, search_char_locs)
 }
 
 fn get_coords_from_ranges(xs: &[i32], ys: &[i32]) -> Vec<CoOrd> {
@@ -101,10 +101,43 @@ fn part_one(parsed: &Parsed) -> usize {
         .count()
 }
 
+fn forms_valid_mas(map: &HashMap<CoOrd, char>, a_loc: &CoOrd) -> bool {
+    let (x, y) = a_loc;
+
+    let up_left = map.get(&(x - 1, y - 1)).unwrap_or(&'!');
+    let up_right = map.get(&(x + 1, y - 1)).unwrap_or(&'!');
+    let down_left = map.get(&(x - 1, y + 1)).unwrap_or(&'!');
+    let down_right = map.get(&(x + 1, y + 1)).unwrap_or(&'!');
+
+    let mut forms_left_to_right = false;
+    let mut forms_right_to_left = false;
+
+    if (*up_left == 'M' && *down_right == 'S') || (*up_left == 'S' && *down_right == 'M') {
+        forms_left_to_right = true;
+    }
+    if (*up_right == 'M' && *down_left == 'S') || (*up_right == 'S' && *down_left == 'M') {
+        forms_right_to_left = true;
+    }
+
+    forms_left_to_right && forms_right_to_left
+}
+
+fn part_two(parsed: &Parsed) -> usize {
+    let (map, a_locs) = parsed;
+    a_locs
+        .iter()
+        .filter(|a_loc| forms_valid_mas(map, a_loc))
+        .count()
+}
+
 fn main() {
     let demo_input = false;
-    let parsed = parse(&demo_input);
 
-    let part_one_answer = part_one(&parsed);
+    let parsed_one = parse(&demo_input, &'X');
+    let part_one_answer = part_one(&parsed_one);
     assert_and_print(&part_one_answer, (18, 2_390), &demo_input);
+
+    let parsed_two = parse(&demo_input, &'A');
+    let part_two_answer = part_two(&parsed_two);
+    assert_and_print(&part_two_answer, (9, 1809), &demo_input);
 }
